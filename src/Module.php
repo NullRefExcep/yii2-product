5cf4b74589e41b68d90bbb9b73c8321a34c88d93
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-07-01 19:51:15 +0300

    add index action view

--- src/Module.php
+++ src/Module.php
@@ -2,8 +2,6 @@
 
 namespace nullref\product;
 
-use yii\base\Application;
-use yii\base\BootstrapInterface;
 use yii\base\Module as BaseModule;
 
 /**
@@ -11,20 +9,13 @@ use yii\base\Module as BaseModule;
  * @copyright 2015 NRE
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Module extends BaseModule implements BootstrapInterface
+class Module extends BaseModule
 {
+    public $controllerNamespace = 'nullref\product\controllers';
+
     public $productModelClass = 'nullref\\product\\models\\Product';
     public $productQueryModelClass = 'nullref\\product\\models\\ProductQuery';
     public $productSearchModelClass = 'nullref\\product\\models\\ProductSearch';
-
-    /**
-     * Bootstrap method to be called during application bootstrap stage.
-     * @param Application $app the application currently running
-     */
-    public function bootstrap($app)
-    {
-        $path = \Yii::getAlias('@app/views/product');
-    }
 
     public function createModel()
     {
---
+++ src/views/main/create.php
@@ -0,0 +1,6 @@
+<?php
+/** @var $this \yii\web\View */
+?>
+<div class="product-create">
+
+</div>
